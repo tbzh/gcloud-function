d2d554686e0a3b6a1928a3b826cbb06b18a6fd70
Tristan <29161072+trisbman@users.noreply.github.com>
2024-12-08 14:39:32 +0700

    add local dev

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const isDev = process.env.NODE_ENV !== 'production';
+const fs = require('fs');
+
 const functions = require('@google-cloud/functions-framework');
 // Import the required modules
 const { Storage } = require('@google-cloud/storage');
@@ -11,13 +14,17 @@ functions.http('helloHttp', (req, res) => {
   res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
 });
 
-functions.http('screenshot', async (req, res) => {    
+functions.http('screenshot', async (req, res) => {
+  const result = await screenshot(req.query.url);
+  const status = result.error ? 500 : 200;
+  res.status(status).send(result);
+});
+
+// Google Cloud Function
+async function screenshot(url = 'https://www.google.com') {
   try {
     // Placeholder for Google Cloud Storage bucket name (to be filled)
     const bucketName = 'b-2443'; // GCS bucket name
-
-    // Extract the URL from the request or default to 'https://www.google.com'
-    const url = req.query.url || 'https://www.google.com';
 
     // Parse the URL to extract the hostname
     const parsedUrl = new URL(url);
@@ -26,10 +33,12 @@ functions.http('screenshot', async (req, res) => {
     // Construct the GCS filename using the hostname
     const gcsKey = `screenshots/${hostname}.png`;
 
-    console.log(url, parsedUrl, hostname)
+    const localChromePath = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
+    const executablePath = isDev ? localChromePath : await chromium.executablePath();
+
     // Launch a headless Chrome browser using Puppeteer with no-sandbox argument
     const browser = await puppeteer.launch({
-      executablePath: await chromium.executablePath(),
+      executablePath,
       args: [...chromium.args, '--no-sandbox'],
       headless: true
     });
@@ -46,16 +55,23 @@ functions.http('screenshot', async (req, res) => {
     // Close the browser
     await browser.close();
 
-    // Upload the screenshot to Google Cloud Storage
-    await storage.bucket(bucketName).file(gcsKey).save(screenshotBuffer, {
-      contentType: 'image/png',
-    });
+    if (isDev) {
+      // Save the screenshot to the local filesystem
+      fs.writeFileSync('screenshot.png', screenshotBuffer);
+    } else {
+      // Upload the screenshot to Google Cloud Storage
+      await storage.bucket(bucketName).file(gcsKey).save(screenshotBuffer, {
+        contentType: 'image/png',
+      });
 
-    console.log(`Screenshot uploaded to ${bucketName}/${gcsKey}`);
-    res.status(200).send(`Screenshot uploaded to ${bucketName}/${gcsKey}`);
+    }
+
+    return { message: `Screenshot uploaded to ${bucketName}/${gcsKey}` }
   } catch (error) {
     // Handle and log errors
     console.error('Error:', error);
-    res.status(500).send('Error: ' + error.toString());
+    return { error: error.toString() }
   }
-});
+};
+
+screenshot();
